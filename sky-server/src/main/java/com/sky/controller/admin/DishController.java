44d20081e414f93f2db9d0d7fbe2547ef4fc3fdc
lantin <xzc1693813596@163.com>
2024-03-05 10:07:17 +0800

    缓存菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,9 +12,15 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+
+/**
+ * 需要保证Redis和数据库中的菜品数据的一致性，所以在新增、修改、删除、启停售菜品的时候，需要同时清除Redis中对应分类的菜品缓存数据
+ */
 
 @Slf4j
 @RequestMapping("/admin/dish")
@@ -24,6 +30,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
     /**
@@ -37,6 +45,10 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品，dishDTO={}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        // 清理redis缓存数据，只清除新增的菜品所属分类的缓存数据即可
+        clearRedisCache("dish_" + dishDTO.getCategoryId());
+
         return Result.success();
     }
 
@@ -65,6 +77,12 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {    // @RequestParam注解用于接收请求参数，spring MVC会自动解析传入的字符串，根据逗号分隔然后赋给集合ids的元素
         log.info("批量删除菜品，ids={}", ids);
         dishService.deleteBatch(ids);
+
+        // 这里就不用再查数据库到底需要清理哪些分类的菜品缓存数据了，直接将所有分类的菜品缓存数据全部清理掉即可
+        // String key = "dish_*";  // 这样写不对，因为删除时是不识别通配符的
+        // 需要先将要删除的key查出来，然后再删除
+        clearRedisCache("dish_*");
+
         return Result.success();
     }
 
@@ -93,6 +111,11 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品信息，dishDTO={}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        // 修改菜品信息如果修改了分类，则会影响到两个分类的菜品缓存数据，这里判断的逻辑比较复杂
+        // 考虑到修改菜品信息是一个比较少的操作，所以这里直接将所有分类的菜品缓存数据全部清理掉即可
+        clearRedisCache("dish_*");
+
         return Result.success();
     }
 
@@ -121,9 +144,22 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("菜品启售停售：status={}, id={}", status, id);
         dishService.startOrStop(status, id);
+
+        // 如果想精确清理还需要做额外的sql查询菜品分类id，这里直接清理所有分类的菜品缓存数据
+        clearRedisCache("dish_*");
+
         return Result.success();
     }
 
 
+    /**
+     * 清理Redis中符合pattern的key的缓存数据
+     * 将这部分代码抽取出来，提高代码的复用性
+     * @param pattern
+     */
+    private void clearRedisCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
 
 }
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 根据分类id查询菜品
@@ -31,12 +34,25 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        log.info("根据分类id查询菜品，categoryId={}", categoryId);
+        // 可以通过Redis来缓存菜品数据，减少数据库的访问，提高性能
+
+        // 1. 查询Redis中是否存在菜品数据，key的格式为：dish_分类id
+        String key = "dish_" + categoryId;
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);    // 放进去的是什么类型的对象，取出来就是什么类型的
+
+        // 2. 如果Redis中存在菜品数据，则直接返回，无需查询数据库
+        if (list != null && list.size() > 0) {
+            log.info("从Redis中查询到了菜品数据：{}", list);
+            return Result.success(list);
+        }
+
+        // 3. 如果Redis中不存在菜品数据，则查询数据库，然后将查询到的菜品数据存入Redis中
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
-
-        List<DishVO> list = dishService.listWithFlavor(dish);
-
+        list = dishService.listWithFlavor(dish);
+        redisTemplate.opsForValue().set(key, list);    // 将查询到的菜品数据存入Redis中
         return Result.success(list);
     }
 
